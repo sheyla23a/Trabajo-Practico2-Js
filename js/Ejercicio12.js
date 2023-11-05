924bf7f43d8bc6c1fdf7728be329e0deb3aa286f
sheyla astorga <sheylaastorga1998@gmail.com>
2023-11-05 12:00:00 -0300

    agregue consigna de ejercicio 12 del tp2js y lo resolvi utilizando math.floor y math.random

---
+++ js/Ejercicio12.js
@@ -0,0 +1,5 @@
+// 12- Realiza un script que genere un número aleatorio entre 1 y 99
+
+
+let numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+document.write("El número aleatorio es: " + numeroAleatorio);
