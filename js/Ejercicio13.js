793795f76301490c5678f8135b8921a7c0b30ee0
sheyla astorga <sheylaastorga1998@gmail.com>
2023-11-05 12:18:58 -0300

    agregue consigna de ejercicio 13 y lo resolvi

---
+++ js/Ejercicio13.js
@@ -0,0 +1,7 @@
+// 13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+
+let texto = prompt("Por favor, ingresa un texto:");
+let textoEnMayusculas = texto.toUpperCase();
+alert("El texto en mayúsculas es: " + textoEnMayusculas);
+
